3266161c023642bac558cc08db3445ebcc0d3e7d
stefanvanbree <stefan.schouteden@gmail.com>
2019-10-19 07:06:18 +0200

    Reusable component for the results

---
+++ src/components/ResultsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+
+const ResultsList = () => {
+  return (
+    <View>
+        <Text>Results List</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultsList
