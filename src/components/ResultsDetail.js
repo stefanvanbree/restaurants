32344ef4f81419a5eeb03f0d199e7eaeea448c26
stefanvanbree <stefan.schouteden@gmail.com>
2019-10-21 20:56:46 +0200

    Start with reusable component for details.

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const ResultsDetail = () => {
+  return (
+    <View>
+        <Text>Results Detail</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultsDetail
